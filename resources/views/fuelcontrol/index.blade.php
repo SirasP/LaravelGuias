21c00543b4ba4940ececc5075340c90fc96f46d9
sebastian <s.lopez.epple@gmail.com>
2026-02-04 15:04:42 -0300

    se agrega el dashboard Fuelcontrol

--- resources/views/fuelcontrol/index.blade.php
+++ resources/views/fuelcontrol/index.blade.php
@@ -18,7 +18,7 @@
     {{-- CONTENIDO PRINCIPAL --}}
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 space-y-8 gap-3 ">
 
-        
+
         <!-- TARJETAS DE RESUMEN -->
         <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mb-4">
             <!-- Productos -->
@@ -99,7 +99,7 @@
 
         <!-- STOCK ACTUAL -->
         <div class="bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden mb-4">
-            
+
             <div class="px-6 py-4  border-b border-gray-200 dark:border-gray-700 ">
                 <div class="flex items-center justify-between">
                     <div class="flex items-center gap-2">
@@ -173,7 +173,8 @@
                                                     d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
                                             </svg>
                                         </div>
-                                        <div class="ml-4">
+                                        <div class="flex items-center gap-4">
+
                                             <div class="text-sm font-medium text-gray-900 dark:text-white">
                                                 {{ $p->nombre }}
                                             </div>
