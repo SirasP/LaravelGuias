a8595ace92110c693f0999c94a6589dbd24e40f6
sebastian <s.lopez.epple@gmail.com>
2026-02-04 15:45:01 -0300

    agrega controller y index de productos

--- resources/views/fuelcontrol/productos/index.blade.php
+++ resources/views/fuelcontrol/productos/index.blade.php
@@ -24,10 +24,10 @@
         <div class="flex flex-col sm:flex-row gap-4 justify-between items-start sm:items-center mb-4">
 
 
-            <div class="flex gap-4 w-full sm:w-auto ">
+            <div class="flex gap-6 w-full sm:w-auto ">
                 <a href="{{ route('fuelcontrol.productos.create') }}"
-                    class="flex items-center justify-center gap-2 px-4 py-2.5 bg-blue-600 text-white rounded-lg text-sm font-medium hover:bg-blue-700 transition-colors shadow-sm w-full sm:w-auto">
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    class="flex items-center justify-center gap-2 px-4 py-3.5 bg-blue-600 text-white rounded-lg text-sm font-medium hover:bg-blue-700 transition-colors shadow-sm w-full sm:w-auto">
+                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
                     </svg>
                     Nuevo Producto
