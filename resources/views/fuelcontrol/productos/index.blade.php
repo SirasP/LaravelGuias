f8f7d22628a65759cce9632c63d82d173c83fcaa
sebastian <s.lopez.epple@gmail.com>
2026-02-04 17:55:11 -0300

    agrega controller y index de productos

--- resources/views/fuelcontrol/productos/index.blade.php
+++ resources/views/fuelcontrol/productos/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div x-data="{ open: false, deleteId: null, createOpen: false, loading: false, nombre: '', cantidad: '' }">
 
-        
+
 
         <x-slot name="header">
             <div class="flex items-center justify-between ">
@@ -269,14 +269,15 @@
                                                     Comienza agregando tu primer producto de combustible
                                                 </p>
                                             </div>
-                                            <a href="{{ route('fuelcontrol.productos.create') }}"
+                                            <button @click="createOpen = true"
                                                 class="mt-2 inline-flex items-center gap-2 px-4 py-2 bg-blue-600 text-white rounded-lg text-sm font-medium hover:bg-blue-700 transition-colors">
+
                                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                         d="M12 4v16m8-8H4" />
                                                 </svg>
-                                                Crear Primer Producto
-                                            </a>
+                                                Nuevo Producto
+                                            </button>
                                         </div>
                                     </td>
                                 </tr>
