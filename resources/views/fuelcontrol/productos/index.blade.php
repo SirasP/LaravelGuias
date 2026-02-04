fb046f880572da77926b0430fdd914a17b1f3eec
sebastian <s.lopez.epple@gmail.com>
2026-02-04 15:48:41 -0300

    agrega controller y index de productos

--- resources/views/fuelcontrol/productos/index.blade.php
+++ resources/views/fuelcontrol/productos/index.blade.php
@@ -37,7 +37,7 @@
 
         <!-- ESTADÍSTICAS RÁPIDAS -->
         @if($productos->isNotEmpty())
-            <div class="grid grid-cols-1 sm:grid-cols-3 gap-4">
+            <div class="grid grid-cols-2 sm:grid-cols-3 gap-4">
                 <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
 
                     @foreach ($productos as $p)
