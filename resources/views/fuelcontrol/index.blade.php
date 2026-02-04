c8534ffd3cbbdd06f3f2e6c995c3695a49f56a65
sebastian <s.lopez.epple@gmail.com>
2026-02-04 15:08:52 -0300

    se agrega el dashboard Fuelcontrol

--- resources/views/fuelcontrol/index.blade.php
+++ resources/views/fuelcontrol/index.blade.php
@@ -173,11 +173,8 @@
                                                     d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
                                             </svg>
                                         </div>
-                                        <div class="flex items-center gap-4">
-
-                                            <div class="text-sm font-medium gap-4 text-gray-900 dark:text-white">
-                                                {{ $p->nombre }}
-                                            </div>
+                                        <div class="ml-4 text-sm font-medium text-gray-900 dark:text-white">
+                                            {{ $p->nombre }}
                                         </div>
                                     </div>
                                 </td>
