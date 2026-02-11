21a49b03033f31a798ceaf44b9ba084450d3bc64
sebastian <s.lopez.epple@gmail.com>
2026-02-11 12:09:07 -0300

    a

--- resources/views/fuelcontrol/xml/modal.blade.php
+++ resources/views/fuelcontrol/xml/modal.blade.php
@@ -2,6 +2,7 @@
     <h2 class="text-lg font-semibold mb-2">📄 XML recibido</h2>
 
     <pre class="bg-gray-900 text-green-300 text-xs p-3 rounded max-h-[60vh] overflow-auto">
-{{ htmlspecialchars($xml->contenido) }}
+{{ htmlspecialchars($xml->archivo_xml) }}
+
     </pre>
 </div>
