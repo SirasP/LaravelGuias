6d5e340eef34534f33c8a6fa1a8b757661d9cd3e
sebastian <s.lopez.epple@gmail.com>
2026-03-20 12:54:11 -0300

    a

--- resources/views/fuelcontrol/movimientos/detalle.blade.php
+++ resources/views/fuelcontrol/movimientos/detalle.blade.php
@@ -21,7 +21,7 @@
     </x-slot>
 
     <div class="py-6 sm:py-8">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
+        <div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 space-y-6">
             
             {{-- Resumen y Gráfico --}}
             <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
